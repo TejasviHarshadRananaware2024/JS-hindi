d6da9ec9aebbe9bcc98f468d445b9e84e6369840
TejasviHarshadRananaware2024 <tejasvi.bhosale944@gmail.com>
2025-03-31 09:37:49 +0000

    Changes for variable concepts

---
+++ .devcontainer/01_basic/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=1234567890
+let accountEmail="tejasvi.bhosale944@gmail.com"
+var accountPassword="12345"
+accountCity="Mumbai"
+accountID=2
+accountEmail="Test@gmail.com"
+accountPassword="Test@1234"
+let accountState;
+console.log(accountId,accountEmail,accountPassword);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
