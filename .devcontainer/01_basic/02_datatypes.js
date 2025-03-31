8882650c0a9425d9084f6c678f94cd6a8b10b8ed
TejasviHarshadRananaware2024 <tejasvi.bhosale944@gmail.com>
2025-03-31 10:03:31 +0000

    datatypes learning

---
+++ .devcontainer/01_basic/02_datatypes.js
@@ -0,0 +1,21 @@
+"Use strict" //treat all code as newer version
+//alert(3+3); we are using node JS not brawsers
+console.log(3
+    +3)//code redablity should be high
+console.log("Tejasvi")
+let accountTest;
+let age=28
+let name="Harshad"
+let state=null
+
+//Number 2 to power 3
+//bigint
+//string
+//boolean=>true/false
+//undefined
+//null is type ,satandalone value
+//symbol==>unique ness data types
+//object
+console.log(typeof"Tejasvi")
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
